c2ccb8c9ed9194e81bb869d783059aa5ad38259d
KudoLayton <lhslhg@gmail.com>
2024-09-22 15:26:54 +0900

    feat: fix cmp and copilot

---
+++ lua/custom/plugins/cmp.lua
@@ -0,0 +1,59 @@
+return {
+  'hrsh7th/nvim-cmp',
+  dependencies = {
+    'zbirenbaum/copilot-cmp',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+  },
+  config = function()
+    local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
+
+    local has_words_before = function()
+      -- if vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt' then
+      if vim.api.nvim_get_option_value('buftype', { buf = 0 }) == 'prompt' then
+        return false
+      end
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match '^%s*$' == nil
+    end
+
+    cmp.setup {
+      mapping = cmp.mapping.preset.insert {
+        ['<CR>'] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        },
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() and has_words_before() then
+            cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      },
+      sources = {
+        { name = 'copilot', priority = 50 },
+        {
+          name = 'lazydev',
+          -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+          group_index = 0,
+        },
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'path' },
+      },
+    }
+  end,
+}
